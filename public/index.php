e77ee22d9f997704958bad49a51f4f08947bb2c9
Bastien <bastien.nicoud@gmail.com>
2017-03-09 14:00:00 +0100

    v0.25

--- public/index.php
+++ public/index.php
@@ -2,3 +2,39 @@
 
   // Fichier d'entrée de l'application
   // contient le routeur vers mes différentes pages
+
+?>
+
+<!doctype html>
+<html lang="fr">
+<html>
+  <head>
+
+    <!-- Charset et wiewport -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Mots clefs et description -->
+    <meta name="keywords" content="Keys">
+    <meta name="description" content="Description">
+
+    <!-- Titre et favicon -->
+    <title>TECHNOTE</title>
+    <link rel="icon" type="image/png" href="img/favicon.png">
+
+    <!-- Styles CSS (normalize + grille bootstrap) -->
+    <link rel="stylesheet" href="css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="css/app.css">
+
+  </head>
+  <body>
+
+
+
+    <!-- Diférents scripts -->
+    <script src="#"></script>
+
+  </body>
+</html>
