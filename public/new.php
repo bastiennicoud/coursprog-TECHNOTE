f7a51065793de83b94831fccb6f731e73f5b83a7
Bastien <bastien.nicoud@gmail.com>
2017-03-15 13:27:38 +0100

    new technote

--- public/new.php
+++ public/new.php
@@ -64,41 +64,14 @@
     <div class="container">
 
       <div class="row top-40">
-        <div class="col-10">
-          <h1>Vos fiches techniques</h1>
-        </div>
-        <div class="col-2">
-          <a class="btn btn-primary float-right" href="new.php">Nouvelle</a>
+        <div class="col-12">
+          <h1>Nouvelle fiche technique</h1>
         </div>
       </div>
 
       <div class="row">
         <div class="col-12">
           <p>Voici la liste de toutes vos fiches techniques!</p>
-        </div>
-      </div>
-
-      <div class="row top-40">
-        <div class="col-12">
-          <div class="card-columns">
-
-            <div class="card">
-              <img class="card-img-top img-fluid" src="img/live.JPG" alt="Card image cap">
-              <div class="card-block">
-                <h4 class="card-title">Phill Collins</h4>
-                <p class="card-text">Une brève description de quelques mots</p>
-                <p class="card-text">Code pin <span class="badge badge-default">5674</span></p>
-                <p class="card-text">Lien de partage : <a href="#" class="card-link">link</a></p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-              </div>
-
-              <div class="card-footer">
-                <a href="#" class="btn btn-primary">Editer</a>
-                <a href="#" class="btn btn-danger">Supprimer</a>
-              </div>
-            </div>
-
-          </div>
         </div>
       </div>
 
