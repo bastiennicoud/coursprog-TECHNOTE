a98ccc3af7d905539db2a1c0d42cc05fc653915f
Bastien <bastien.nicoud@gmail.com>
2017-03-15 12:14:06 +0100

    cards

--- public/dashboard.php
+++ public/dashboard.php
@@ -59,6 +59,40 @@
       </div>
     </nav>
 
+
+
+    <div class="container">
+
+      <div class="row">
+        <div class="col-12">
+          <h1>Vos fiches techniques</h1>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="card-columns">
+
+          <div class="card">
+            <img class="card-img-top img-fluid" src="img/live.JPG" alt="Card image cap">
+            <div class="card-block">
+              <h4 class="card-title">Phill Collins</h4>
+              <p class="card-text">Une brève description de quelques mots</p>
+              <p class="card-text">Code pin <span class="badge badge-default">5674</span></p>
+              <p class="card-text">Lien de partage : <a href="#" class="card-link">link</a></p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+
+            <div class="card-footer">
+              <a href="#" class="btn btn-primary">Editer</a>
+              <a href="#" class="btn btn-danger">Supprimer</a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+
     <!-- Diférents scripts -->
     <script src="js/jquery.js"></script>
     <script src="js/tether/js/tether.js"></script>
