66bab0a7f553809b411efe54726ba7d7f40ff822
Bastien <bastien.nicoud@gmail.com>
2017-03-17 12:26:40 +0100

    edit

--- public/edit.php
+++ public/edit.php
@@ -46,16 +46,6 @@
           </li>
         </ul>
 
-        <form class="form-inline my-2 my-lg-0">
-          <div class="input-group">
-            <input class="form-control" type="text" placeholder="Rechercher">
-            <span class="input-group-btn">
-              <button class="btn btn-primary" type="button">Go!</button>
-            </span>
-          </div>
-
-        </form>
-
       </div>
     </nav>
 
@@ -64,46 +54,18 @@
     <div class="container">
 
       <div class="row top-40">
-        <div class="col-10">
-          <h1>Vos fiches techniques</h1>
-        </div>
-        <div class="col-2">
-          <a class="btn btn-primary float-right" href="new.php">Nouvelle</a>
+        <div class="col-12">
+          <h1>Editer votre fiche technique</h1>
         </div>
       </div>
 
       <div class="row">
-        <div class="col-12">
-          <p>Voici la liste de toutes vos fiches techniques!</p>
-        </div>
-      </div>
-
-      <div class="row top-40">
-        <div class="col-12">
-          <div class="card-columns">
-
-            <div class="card">
-              <img class="card-img-top img-fluid" src="img/live.JPG" alt="Card image cap">
-              <div class="card-block">
-                <h4 class="card-title">Phill Collins</h4>
-                <p class="card-text">Une brève description de quelques mots</p>
-                <p class="card-text">Code pin <span class="badge badge-default">5674</span></p>
-                <p class="card-text">Lien de partage : <a href="#" class="card-link">link</a></p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-              </div>
-
-              <div class="card-block">
-                  <a href="#" class="btn btn-info">Prévisualiser</a>
-              </div>
-
-              <div class="card-footer">
-                <a href="#" class="btn btn-primary">Editer</a>
-                <a href="#" class="btn btn-danger">Supprimer</a>
-              </div>
-            </div>
-
-          </div>
-        </div>
+        <button type="button" name="button" class="btn btn-info">Editer les informations</button>
+        <button type="button" name="button" class="btn btn-info">Editer les contatcs</button>
+        <button type="button" name="button" class="btn btn-info">Editer les commentaires</button>
+        <button type="button" name="button" class="btn btn-info">Ajouter un plan de scene</button>
+        <button type="button" name="button" class="btn btn-info">Editer le patch</button>
+        <button type="button" name="button" class="btn btn-info">Editer les musiciens</button>
       </div>
 
     </div>
