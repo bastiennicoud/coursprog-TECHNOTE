01113dff92c21c1dc676911fbec41656032ab8d4
Bastien <bastien.nicoud@gmail.com>
2017-03-26 17:42:47 +0200

    patchlist

--- public/technote.php
+++ public/technote.php
@@ -93,7 +93,82 @@
           </div>
         </div>
 
+        <div class="row top-10">
+
+          <div class="col-sm-12">
+            <div class="box inverse">
+              <h4>Acceuil technique</h4>
+              <p class="font-weight-bold">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+          </div>
+
+          <div class="col-sm-6 top-20">
+            <div class="box inverse">
+              <h4>Acceuil technique</h4>
+              <p class="font-weight-bold">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+            </div>
+          </div>
+
+          <div class="col-sm-6 top-20">
+            <div class="box inverse">
+              <h4>Acceuil technique</h4>
+              <p class="font-weight-bold">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+            </div>
+          </div>
+
+        </div>
+
       </div>
+    </div>
+
+    <div class="container padd-40">
+
+      <div class="row">
+        <div class="col-12">
+          <h2>Patchlist</h2>
+        </div>
+      </div>
+
+      <div class="row top-10">
+        <div class="col-12">
+
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Username</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </table>
+
+        </div>
+      </div>
+
     </div>
 
 
